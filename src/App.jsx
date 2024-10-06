07b2c45135aaabf120d78db42df31987fd16bf94
yashksaini-coder <ys3853428@gmail.com>
2024-10-06 20:29:24 +0530

    refactor: 💡 Interval function breaking app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function App (){
-  return (
+  return(
     <div>
       <Counter></Counter>
     </div>
@@ -12,26 +12,13 @@ function Counter(){
   
   let [count, setCount] = useState(0);
 
-  function increase(){
+  setInterval(() => {
     setCount(count + 1);
-  }
-
-  function decrease(){
-    setCount(count - 1);
-  }
-
-  function reset(){
-    setCount(count = 0);
-  }
+  }, 1000);
 
   return (
     <div>
       <h1 id="text">{count}</h1>
-      <button onClick={increase}>Increase</button>
-      <br></br>
-      <button onClick={decrease}>Decrease</button>
-      <br></br>
-      <button onClick={reset}>Reset</button>
     </div>
   )
 
