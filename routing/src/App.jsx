e826e700eff3244cf25d26c1a3c47300163fa458
yashksaini-coder <ys3853428@gmail.com>
2024-10-12 21:01:19 +0530

    Use Outlet

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,24 +1,36 @@
 
 import './App.css'
-import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 
 function App() {
 
   return <div>
     <BrowserRouter>
-      <Link to="/">Allen</Link>
-      | 
-      <Link to="/neet/online-coaching-class-11">Class 11</Link> 
-      | 
-      <Link to="/neet/online-coaching-class-12">Class 12</Link>
       <Routes>
-        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-        <Route path="/" element={<Landing />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path='/' element={<layout/>}>
+            <Route path="/" element={<Landing />} />
+            <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+            <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
     </BrowserRouter>
   </div>
+}
+
+function Layout() {
+  return (
+    <div style={{ height: '100vh' }}>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/neet/online-coaching-class-11">Class 11</Link>
+        <Link to="/neet/online-coaching-class-12">Class 12</Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
 function Landing() {
