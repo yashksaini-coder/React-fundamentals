825e32b85e88f59b6f5b91c8d2ade2ca8c0cd624
yashksaini-coder <ys3853428@gmail.com>
2024-10-12 20:38:20 +0530

    Performed basic routing + SPA

---
+++ routing/src/App.jsx
@@ -0,0 +1,41 @@
+
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+
+  return <div>
+    <BrowserRouter>
+      <Link to="/">Allen</Link>
+      | 
+      <Link to="/neet/online-coaching-class-11">Class 11</Link> 
+      | 
+      <Link to="/neet/online-coaching-class-12">Class 12</Link>
+      <Routes>
+        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+}
+
+function Landing() {
+  return <div>
+    Welcome to allen
+  </div>
+}
+
+function Class11Program() {
+  return <div>
+      NEET programs for Class 11th
+  </div>
+}
+
+function Class12Program() {
+  return <div>
+      NEET programs for Class 12th
+  </div>
+}
+
+export default App
---
+++ routing/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
