6ad6fc883db76ef6ebe84bce78aa040367cc7a2e
yashksaini-coder <ys3853428@gmail.com>
2024-10-13 20:18:58 +0530

    performed state experiments

---
+++ state-experiments/src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <LightBulb />
+    </div>
+  )
+}
+
+BulbState.propTypes = {
+  bulbOn: PropTypes.bool.isRequired,
+};
+
+function BulbState({ bulbOn }) {
+  return <h2>{bulbOn ? "Bulb is on" : "Bulb is off"}</h2>;
+}
+
+function LightBulb() {
+    const [bulbOn, setBulbOn] = useState(true);
+    return (
+        <>
+            <BulbState bulbOn={bulbOn} />
+            <ToggleBulb setBulbOn={setBulbOn} />
+        </>
+    )
+}
+
+ToggleBulb.propTypes = {
+    setBulbOn: PropTypes.func.isRequired,
+};
+
+function ToggleBulb({ setBulbOn }) {
+    return <button onClick={() => setBulbOn(prev => !prev)}>Toggle Bulb</button>;
+}
+
+export default App
